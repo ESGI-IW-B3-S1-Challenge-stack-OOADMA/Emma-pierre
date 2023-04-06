20c20b3982ffd2f71c985bb6318ebf031a4d68d8
Mathis Rome <mathis.rome@icloud.com>
2023-04-06 19:22:01 +0200

    fix : correction navbar mobile

--- javascripts/components/navbar.js
+++ javascripts/components/navbar.js
@@ -3,11 +3,25 @@ function navbar(){
     let navbarLogo = navbar.querySelector('img')
     let navbarButton = navbar.querySelector('button')
     let navbarList = navbar.querySelectorAll('ul')[0]
+    let dropdownLinks = navbar.querySelectorAll('li.dropdown a')
 
     navbarButton.onclick = () => {
         navbarList.classList.toggle('show')
     }
 
+    dropdownLinks.forEach(link => {
+        link.onclick = () => {
+            link.parentElement.classList.toggle('show')
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!Array.from(dropdownLinks).includes(event.target)){
+            dropdownLinks.forEach(link => {
+                link.parentElement.classList.remove('show')
+            });
+        }
+    });
 
     document.addEventListener('scroll', () => {
         if(document.documentElement.scrollTop > 20 ) {
