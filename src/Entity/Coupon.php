e7e088e3c6e59483aee471bea4f509106c2e330a
Loan <loanbillonnet@gmail.com>
2023-07-19 10:27:41 +0200

    feat: create coupon entity

---
+++ src/Entity/Coupon.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+
+class Coupon
+{
+    private int $id;
+    private string $name;
+    private string $code;
+    private int $percent;
+    private string $duration;
+    private int $duration_in_month;
+    private bool $valid;
+    private DateTimeImmutable $created_at;
+    private DateTimeImmutable $updated_at;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode(string $code): void
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPercent(): int
+    {
+        return $this->percent;
+    }
+
+    /**
+     * @param int $percent
+     */
+    public function setPercent(int $percent): void
+    {
+        $this->percent = $percent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDuration(): string
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param string $duration
+     */
+    public function setDuration(string $duration): void
+    {
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDurationInMonth(): int
+    {
+        return $this->duration_in_month;
+    }
+
+    /**
+     * @param int $duration_in_month
+     */
+    public function setDurationInMonth(int $duration_in_month): void
+    {
+        $this->duration_in_month = $duration_in_month;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid(): bool
+    {
+        return $this->valid;
+    }
+
+    /**
+     * @param bool $valid
+     */
+    public function setValid(bool $valid): void
+    {
+        $this->valid = $valid;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param DateTimeImmutable $created_at
+     */
+    public function setCreatedAt(DateTimeImmutable $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getUpdatedAt(): DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param DateTimeImmutable $updated_at
+     */
+    public function setUpdatedAt(DateTimeImmutable $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+}
