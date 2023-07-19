eaeb3de082b9308238032d75627b653fcea5a9fa
Loan <loanbillonnet@gmail.com>
2023-07-19 19:49:10 +0200

    feat: user favorites list
    fix backend bugs + frontend

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,7 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\FavoriteRepository;
+use App\Routing\Attribute\Route;
+use Exception;
+
 class FavoriteController extends AbstractController
 {
+    /**
+     * @throws Exception
+     */
+    #[Route('/favorites', name: 'favorites_show')]
+    public function listByUser(FavoriteRepository $favoriteRepository)
+    {
+        if (!$this->getSession()->has('user_id')) {
+            return $this->redirectToRoute('/login');
+        }
 
+        $favorites = $favoriteRepository->findAllByUser($this->getUser()->getId());
+        return $this->render('_favoris.html.twig', [
+            'favorites' => $favorites
+        ]);
+    }
 }
--- src/Repository/FavoriteRepository.php
+++ src/Repository/FavoriteRepository.php
@@ -28,17 +28,17 @@ class FavoriteRepository extends AbstractRepository
                 u.phone_number user_phone_number, u.password user_password, u.roles user_roles, u.created_at user_created_at,
                 u.updated_at user_updated_at, p.id product_id, p.name product_name, p.description product_description,
                 pc.id product_product_category_id, pc.name product_product_category_name, pc.created_at product_product_category_created_at,
-                pc.updated_at product_product_category_updated_at, jw.id product_jewelry_category_id, jc.name product_jewelry_category_name,
+                pc.updated_at product_product_category_updated_at, jc.id product_jewelry_category_id, jc.name product_jewelry_category_name,
                 jc.created_at product_jewelry_category_created_at, jc.updated_at product_jewelry_category_updated_at,
-                p.price product_price, p.available product_available, p.created_at product_created_at, p.updated_at product_updated_at
+                p.price product_price, p.available product_available, p.created_at product_created_at, p.updated_at product_updated_at,
                 f.created_at product_created_at, p.updated_at product_updated_at
             FROM favorite f 
             INNER JOIN `user` u ON f.user_id = u.id
             INNER JOIN product p ON f.product_id = p.id
             INNER JOIN product_category pc ON p.product_category_id = pc.id
             INNER JOIN jewelry_category jc ON p.jewelry_category_id = jc.id
-            ORDER BY f.created_at
-            WHERE f.user_id = ?;
+            WHERE f.user_id = ?
+            ORDER BY f.created_at;
         ';
         $statement = $this->pdo->prepare($sql);
         $statement->execute([$user_id]);
@@ -47,6 +47,8 @@ class FavoriteRepository extends AbstractRepository
         if ($data === false) {
             return [];
         }
+
+        $favorites = [];
 
         foreach ($data as $favorite) {
             $favoriteDta = new FavoriteDTA($favorite);
