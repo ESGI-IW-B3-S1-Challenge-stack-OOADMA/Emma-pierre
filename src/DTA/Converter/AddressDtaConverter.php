5163ab9abe8fe4bd9656115913206944b1e72423
Loan <loanbillonnet@gmail.com>
2023-07-19 11:15:38 +0200

    feat: create AddressDtaConverter

---
+++ src/DTA/Converter/AddressDtaConverter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DTA\Converter;
+
+use App\DTA\AddressDTA;
+use App\Entity\Address;
+
+class AddressDtaConverter
+{
+    public function toAddress(AddressDTA $addressDTA): Address
+    {
+        $address = new Address();
+        $address->setId($addressDTA->id);
+        $address->setName($addressDTA->name);
+        $address->setAddressLine1($addressDTA->address_line1);
+        $address->setAddressLine2($addressDTA->address_line2);
+        $address->setCity($addressDTA->city);
+        $address->setPostalCode($addressDTA->postal_code);
+        $address->setCountry($addressDTA->country);
+        $address->setCreatedAt($addressDTA->created_at);
+        $address->setUpdatedAt($addressDTA->updated_at);
+        return $address;
+    }
+}
