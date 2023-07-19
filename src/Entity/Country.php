e4457dd2feccb4e845941aca7ce728ad8275bdb4
Loan <loanbillonnet@gmail.com>
2023-07-19 10:41:09 +0200

    feat: add Country entity getters & setters

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -2,10 +2,76 @@
 
 namespace App\Entity;
 
+use DateTimeImmutable;
+
 class Country
 {
     private int $id;
     private string $name;
-    private \DateTimeImmutable $created_at;
-    private \DateTimeImmutable $updated_at;
+    private DateTimeImmutable $created_at;
+    private DateTimeImmutable $updated_at;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param DateTimeImmutable $created_at
+     */
+    public function setCreatedAt(DateTimeImmutable $created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getUpdatedAt(): DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param DateTimeImmutable $updated_at
+     */
+    public function setUpdatedAt(DateTimeImmutable $updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
 }
