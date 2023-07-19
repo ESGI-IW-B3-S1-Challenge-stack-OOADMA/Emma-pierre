b0806ac158adb9f53882bf2f50fff721c55bfcc8
Loan <loanbillonnet@gmail.com>
2023-07-19 10:56:23 +0200

    feat: create CountryDtaConverter

---
+++ src/DTA/Converter/CountryDtaConverter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DTA\Converter;
+
+use App\DTA\CountryDTA;
+use App\Entity\Country;
+
+class CountryDtaConverter
+{
+    public function toCountry(CountryDTA $countryDTA): Country
+    {
+        $country = new Country();
+        $country->setId($countryDTA->id);
+        $country->setName($countryDTA->id);
+        $country->setCode($countryDTA->code);
+        $country->setCreatedAt($countryDTA->created_at);
+        $country->setUpdatedAt($countryDTA->updated_at);
+        return $country;
+    }
+}
